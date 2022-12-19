8ed711598ae4cc480fe92fbed29009cfbabf6f38
Julian Baranowski <julian.paul.baranowski@gmail.com>
2022-12-18 21:50:37 -0700

    added button and linked JS in prep for array

---
+++ neighborhood.js
@@ -0,0 +1,8 @@
+function randomResturant(evt) {
+    evt.preventDefault();
+    alert('website')
+}
+
+let food = document.querySelector('#random-resturant');
+
+food.addEventListener('click', randomResturant);
